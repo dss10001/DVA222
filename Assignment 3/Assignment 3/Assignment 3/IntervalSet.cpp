3df48ce9592b137786aabb6e293f6a9f24d3437a
bobobear91 <rcd10002@student.mdh.se>
2015-05-11 16:14:04 +0200

    update

--- Assignment 3/Assignment 3/Assignment 3/IntervalSet.cpp
+++ Assignment 3/Assignment 3/Assignment 3/IntervalSet.cpp
@@ -3,25 +3,54 @@
 
 IntervalSet::IntervalSet()
 {
+	size = 0;
+	intervalOfInts = new Interval[size];
+
 }
 
 
 IntervalSet::~IntervalSet()
 {
+	delete[]intervalOfInts;
 }
 
 bool IntervalSet::Contains(int elem)
 {
-	throw "The method or operation is not implemented.";
+	for (int i = 0; i < size;i++)
+		if (elem >= intervalOfInts[i].intervalStart && elem <= intervalOfInts[i].intervalEnd)
+			return true;
+	return false;
 }
 
 void IntervalSet::Add(int elem)
 {
-	throw "The method or operation is not implemented.";
+	if (!Contains(elem))
+	{
+		if (size /*== count*/)
+		{
+			ExtendArray(1);
+		}
+		for (int i = 0; i < size; i++)
+		{
+			if (elem >= intervalOfInts[i].intervalStart && elem <= intervalOfInts[i].intervalEnd)
+			{
+
+			}
+		}
+		for (int i = 0; i < size; i++)
+		{
+
+		}
+	}
 }
 
 void IntervalSet::ExtendArray(int value)
-{}
+{
+	Interval *newInterval = new Interval[size + value];
+	memcpy(newInterval, intervalOfInts, sizeof(Interval));
+	delete []intervalOfInts;
+	intervalOfInts = newInterval;
+}
 
 IIntSet* IntervalSet::Union(IIntSet &other)
 {
--- Assignment 3/Assignment 3/Assignment 3/IntervalSet.h
+++ Assignment 3/Assignment 3/Assignment 3/IntervalSet.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdio.h>
+#include <iostream>
 #include "IIntSet.h"
 struct Interval
 {
