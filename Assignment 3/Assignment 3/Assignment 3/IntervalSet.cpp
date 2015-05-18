2c7e4070de7cf4ed6a6751b79ffa20e313c776b5
bobobear91 <rcd10002@student.mdh.se>
2015-05-18 14:10:08 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assignment 3/Assignment 3/Assignment 3/UnionIntSet.cpp

--- Assignment 3/Assignment 3/Assignment 3/IntervalSet.cpp
+++ Assignment 3/Assignment 3/Assignment 3/IntervalSet.cpp
@@ -54,10 +54,10 @@ void IntervalSet::ExtendArray(int value)
 
 IIntSet* IntervalSet::Union(IIntSet &other)
 {
-	throw "The method or operation is not implemented.";
+
 }
 
 char* IntervalSet::ToString()
 {
-	throw "The method or operation is not implemented.";
+
 }
